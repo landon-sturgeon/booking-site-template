dfd2ae65843ee0cc24e5d82531591317dbe6638f
Sturgeon <sturgeoj@nala.roche.com>
2020-01-02 12:00:03 -0800

    added basic html layout styling

--- src/pages/landing-page/landing-page.component.js
+++ src/pages/landing-page/landing-page.component.js
@@ -1,8 +1,18 @@
 import React from "react"
 
 const LandingPage = () => (
-    <div className="test">
-        Hello World
+    <div className="container">
+        <header className="header">
+            Header part
+        </header>
+        <div className="content">
+            <nav className="sidebar">
+                Navigation
+            </nav>
+            <main className="hotel-view">
+                Hotel View
+            </main>
+        </div>
     </div>
 )
 
